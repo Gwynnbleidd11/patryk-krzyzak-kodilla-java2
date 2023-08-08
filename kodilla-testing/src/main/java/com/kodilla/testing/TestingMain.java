b063e17b0e4f246efd9a560d191f93ce8da0ea5e
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2023-08-08 21:13:58 +0200

    6.2. Test Driven Development

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy: ");
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        Calculator calculator = new Calculator();
+
+        int addition = calculator.addAToB(10, 5);
+        if (addition == 15) {
+            System.out.println("Addition test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int subtraction = calculator.subtractAFromB(10, 5);
+        if (subtraction == 5) {
+            System.out.println("Subtraction test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int b, int a) {
+        return b - a;
+    }
+}
