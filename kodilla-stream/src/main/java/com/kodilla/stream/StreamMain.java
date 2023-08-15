83cc49ec85fe768f347cad030dc501b399cc627f
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2023-08-15 22:24:28 +0200

    7.1 Introduction to functional programming

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::divideAByB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::subBFromA);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("This is a nice day", (text) -> "..." + text + "...");
+        poemBeautifier.beautify("This is a nice day", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("This is a nice day", (text) -> "ABC " + text + " ABC");
+        poemBeautifier.beautify("This is a nice day", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("This is a nice day", (text) -> "*** " + text + " @@@");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "kodilla-course2"
 include("kodilla-testing")
+include("kodilla-stream")
