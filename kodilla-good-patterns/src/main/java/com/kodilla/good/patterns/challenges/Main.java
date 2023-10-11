10cc2a0f5ccaa0fcf263313e290397e6d7148350
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2023-10-11 11:32:31 +0200

    13.1 Idea of clean code

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        movieStore.displayMovieTitles();
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println();
+
+        System.out.println(movieStore.factorial(3));
+
+    }
+}
